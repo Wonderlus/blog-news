31c66dffa99c943cd5ebd4433a2a286d334b3284
Wonderlus <iliyawhitelion@gmail.com>
2023-06-26 16:41:26 +0300

    added pub date and source in blog

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -32,8 +32,19 @@ const BlogPage = async () => {
     }
 
 
+    function formateDate(stringDate: string) {
+        const date = new Date(stringDate);
+        const day = date.getDate().toString().padStart(2, "0");
+        const month = (date.getMonth() + 1).toString().padStart(2, "0");
+        const year = date.getFullYear();
+
+        return `${day}.${month}.${year}`;
+        
+    }
+
     if (rss) {
         for (let elem of rss) {
+            
             for (let item of elem.items){
                 const title = item.title;
                 const description = item.content ? item.content :"";
@@ -41,12 +52,13 @@ const BlogPage = async () => {
                 const source = elem.link ? elem.link : elem.feedUrl;
                 const img = item.enclosure ? item.enclosure.url : item.link;
                 const date = item.pubDate;
+                
                 allNews.push({
                     title,
                     description,
                     link,
                     img,
-                    date,
+                    date: date ? formateDate(date) : date,
                     source
                 })
             }
--- src/components/NewsPage.tsx
+++ src/components/NewsPage.tsx
@@ -1,120 +1,128 @@
 "use client";
 import { useEffect, useState } from "react";
-import styles from "./NewsPage.module.scss"
-import {usePathname} from "next/navigation"
+import styles from "./NewsPage.module.scss";
+import { usePathname } from "next/navigation";
 import { useSelector } from "react-redux";
 import { useAppSelector } from "@/store/store";
-import Image from "next/image"
+import Image from "next/image";
 import Link from "next/link";
 
+const NewsPage = ({ allNews }: { allNews: IallNews[] }) => {
+  let sourceNews: IallNews[];
 
-const NewsPage = ({allNews} : {allNews: IallNews[]}) => {
-        
-    let sourceNews: IallNews[];
+  const pathname = usePathname();
+  const currentPage = Number(pathname.slice(1));
+  const [maxLengthLinks, setMaxLengthLinks] = useState(0);
+  const [outputNews, setOutputNews] = useState<IallNews[]>();
 
-    
-    
-    const pathname = usePathname();
-    const currentPage = Number(pathname.slice(1));
-    const [maxLengthLinks, setMaxLengthLinks] = useState(0);
-    const [outputNews, setOutputNews] = useState<IallNews[]>();
+  const view = useAppSelector((state) => state.viewReducer);
+  const search = useAppSelector((state) => state.searchReducer);
+  const source = useAppSelector((state) => state.sourceReducer);
 
-    const view = useAppSelector((state) => state.viewReducer);
-    const search = useAppSelector((state) => state.searchReducer);
-    const source = useAppSelector((state) => state.sourceReducer);
+  console.log(view);
 
-    console.log(view)
+  function getMaxLength() {
+    if (view) {
+      return Math.ceil(sourceNews.length / 4);
+    } else {
+      return Math.ceil(sourceNews.length / 3);
+    }
+  }
 
-    function getMaxLength() {
-        if (view) {
-            return Math.ceil(sourceNews.length / 4);
-        }
-    
-        else  {
-            return Math.ceil(sourceNews.length / 3);
-        }
+  useEffect(() => {
+    if (source) {
+      sourceNews = allNews.filter((news) => news.source === source);
+    } else {
+      sourceNews = allNews.slice();
     }
 
-    
-    useEffect(() => {
-        if (source) {
-            sourceNews = allNews.filter(news => news.source === source);
-        }
+    if (search) {
+      sourceNews = sourceNews.filter(
+        (news) =>
+          news.title!.toLowerCase().includes(search.toLowerCase()) ||
+          news.description!.toLowerCase().includes(search.toLowerCase())
+      );
+    }
 
-        else {
-            sourceNews = allNews.slice();
-        }
-        
-        if (search) {
-            sourceNews = sourceNews.filter(news => news.title!.toLowerCase().includes(search.toLowerCase()) || news.description!.toLowerCase().includes(search.toLowerCase()));
-            
-        }
+    if (view) {
+      setOutputNews(sourceNews.slice((currentPage - 1) * 4, currentPage * 4));
+    } else if (!view) {
+      setOutputNews(sourceNews.slice((currentPage - 1) * 3, currentPage * 3));
+    }
+    setMaxLengthLinks(getMaxLength());
+  }, [search, view, source]);
 
-        
-        if (view) {
-            setOutputNews(sourceNews.slice((currentPage - 1) * 4, (currentPage * 4)));
-        }
-        else if (!view) {
-            setOutputNews(sourceNews.slice((currentPage - 1) * 3, (currentPage * 3)));
-        }
-        setMaxLengthLinks(getMaxLength());
-    }, [search, view, source])
-    
-    
+  console.log(allNews);
 
-    
-    
-    return ( 
-        <div>
-            
-            {view ? 
-                <div className={styles.blog}>
-                    {outputNews?.map((news, index) => (
-                    <div key={index * currentPage}>
-                        <h2 className={styles.title}>{news.title}</h2>
-                        <h5 className={styles.description}>{news.description}</h5>
-                        <a href={news.link} className={styles.readMore}>Подробнее</a>
-                    </div>
-                    ))}
-                </div>       
-                :
-                <div className={styles.blogView}>
-                    {outputNews?.map((news, index) => (
-                        <div key={index * currentPage}>
-                            <Image width={200} height={100} src={news.img!} alt={""} />
-                            <div>
-                            <a href={news.link} className={styles.title}>{news.title}</a>
-                            <h5 className={styles.description}>{news.description}</h5>
-                            </div>
-                        </div> 
-                    ))}
+  return (
+    <div>
+      {view ? (
+        <div className={styles.blog}>
+          {outputNews?.map((news, index) => (
+            <div className={styles.blogNew} key={index * currentPage}>
+              <h2 className={styles.title}>{news.title}</h2>
+              <h5 className={styles.description}>{news.description}</h5>
+              <a href={news.link} className={styles.readMore}>
+                Подробнее
+              </a>
+              <div className={styles.misc}>
+              <Link href={news.source!} className={styles.source}>{news.source === "https://www.mos.ru" ? "mos.ru" : "lenta.ru"}</Link>
+                    <div className={styles.date}>{news.date}</div>
                 </div>
-            }
-            
-            
-
-            {currentPage > 3 ?
-                    <nav className={styles.nav}>
-                        <Link href={"/" + (currentPage - 2)}>{currentPage - 2}</Link>
-                        <Link href={"/" + (currentPage - 1)}>{currentPage - 1}</Link>
-                        <Link className={styles.active} href={"/" + (currentPage)}>{currentPage}</Link>
-                        <Link href={"/" + (currentPage + 1)}>{currentPage + 1}</Link>
-                        ...
-                        <Link href={"/" + maxLengthLinks}>{maxLengthLinks}</Link>
-                    </nav>
-                    :
-                    <nav className={styles.nav}>
-                        <Link className={pathname === "/1" ? styles.active : "" } href={"/1"}>1</Link>
-                        <Link className={pathname === "/2" ? styles.active : "" }  href={"/2"}>2</Link>
-                        <Link className={pathname === "/3" ? styles.active : "" }  href={"/3"}>3</Link>
-                        <Link className={pathname === "/4" ? styles.active : "" }  href={"/4"}>4</Link>
-                        ...
-                        <Link href={"/" + maxLengthLinks}>{maxLengthLinks}</Link>
-                    </nav>
-                }  
+            </div>
+          ))}
         </div>
-     );
-}
- 
+      ) : (
+        <div className={styles.blogView}>
+          {outputNews?.map((news, index) => (
+            <div className={styles.blogNew}  key={index * currentPage}>
+              <Image width={200} height={100} src={news.img!} alt={""} />
+              <div>
+                <a href={news.link} className={styles.title}>
+                  {news.title}
+                </a>
+                <h5 className={styles.description}>{news.description}</h5>
+              </div>
+                <div className={styles.miscView}>
+                    <Link href={news.source!} className={styles.source}>{news.source === "https://www.mos.ru" ? "mos.ru" : "lenta.ru"}</Link>
+                    <div className={styles.date}>{news.date}</div>
+                </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {currentPage > 3 ? (
+        <nav className={styles.nav}>
+          <Link href={"/" + (currentPage - 2)}>{currentPage - 2}</Link>
+          <Link href={"/" + (currentPage - 1)}>{currentPage - 1}</Link>
+          <Link className={styles.active} href={"/" + currentPage}>
+            {currentPage}
+          </Link>
+          <Link href={"/" + (currentPage + 1)}>{currentPage + 1}</Link>
+          ...
+          <Link href={"/" + maxLengthLinks}>{maxLengthLinks}</Link>
+        </nav>
+      ) : (
+        <nav className={styles.nav}>
+          <Link className={pathname === "/1" ? styles.active : ""} href={"/1"}>
+            1
+          </Link>
+          <Link className={pathname === "/2" ? styles.active : ""} href={"/2"}>
+            2
+          </Link>
+          <Link className={pathname === "/3" ? styles.active : ""} href={"/3"}>
+            3
+          </Link>
+          <Link className={pathname === "/4" ? styles.active : ""} href={"/4"}>
+            4
+          </Link>
+          ...
+          <Link href={"/" + maxLengthLinks}>{maxLengthLinks}</Link>
+        </nav>
+      )}
+    </div>
+  );
+};
 
 export default NewsPage;
